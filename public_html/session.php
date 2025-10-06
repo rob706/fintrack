8d2c8c6f968744fcaf81cac9578359bc9a07957e
Rob <rob@czdesignz.uk>
2025-10-06 20:22:39 +0100

    Change unknown user default behaviour

--- public_html/session.php
+++ public_html/session.php
@@ -3,16 +3,16 @@
 @include_once("./config.php");
 session_start();
 if(!isset($_SESSION["email"])){
-header("Location: login.php");
+header("Location: /login.php");
 exit();
 }
 
 $sess_email = $_SESSION["email"];
-$sql = "SELECT user_id, firstname, lastname, email, profile_path FROM users WHERE email = '$sess_email'";
+$sql = "SELECT user_id, firstname, lastname, email, profile_path FROM users WHERE email = '".$sess_email."'";
 
 $result = $con->query($sql);
 
-if ($result->num_rows > 0) {
+if ($result->num_rows != 1) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
     $userid=$row["user_id"];
@@ -23,9 +23,8 @@ if ($result->num_rows > 0) {
     $userprofile="/uploads/".$row["profile_path"];
   }
 } else {
-    $userid="GHX1Y2";
-    $username ="Jhon Doe";
-    $useremail="mailid@domain.com";
-    $userprofile="/Uploads/default_profile.png";
+  session_destroy();
+  header("Location: /login.php");
+  exit();
 }
 ?>
