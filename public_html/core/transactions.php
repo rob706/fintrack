b207d8e477595dbec21f5a39719f2fef78ff356e
Rob <rob.wharmby@gmail.com>
2025-10-07 20:50:51 +0100

    Update logic to migrate categories to the DB
    This will do the amendments and the DB Changelog has the SQL to update the db

--- public_html/core/transactions.php
+++ public_html/core/transactions.php
@@ -8,11 +8,28 @@ if(isset($_POST['trans_amount'])) $trans_amount = $con->real_escape_string($_POS
 if(isset($_POST['trans_date'])) $trans_date = $con->real_escape_string($_POST['trans_date']);
 if(isset($_POST['trans_category'])) $trans_category = $con->real_escape_string($_POST['trans_category']);
 if(isset($_POST['trans_type'])) $trans_type = $con->real_escape_string($_POST['trans_type']);
+if(isset($_POST['customCategory'])) $trans_customcategory = $con->real_escape_string($_POST['customCategory']);
 if(isset($_POST['id'])) $id = $con->real_escape_string($_POST['id']);
 
-$act = $_POST['act'];
+## Flip the Signs on Expenses so they are processed as Negatives
 
 if($trans_type == "expense") $trans_amount = $trans_amount * -1;
+
+## Check if the Custom Category already exists and if not create the category
+
+if(isset($trans_customcategory)){
+    $q_cat = $con->query("select category_id from category where category_name = '".$trans_customcategory."' and user_id in (0,".$userid.") limit 1");
+
+    if($q_cat->num_rows == 0){
+        $con_update->query("insert into category (`category_name`,`user_id`,`".$trans_type."`) values ('".$trans_customcategory."','".$userid."',1);");
+
+        $q_cat = $con->query("select category_id from category where category_name = '".$trans_customcategory."' and user_id in (0,".$userid.") limit 1");
+    }
+    $cat = $q_cat->fetch_assoc();
+    $trans_category = $cat['category_id'];
+} 
+
+$act = $_POST['act'];
 
 switch($act){
     default:
@@ -20,11 +37,11 @@ switch($act){
         break;
 
     case 'add':
-        $sql = "INSERT INTO transactions (user_id, date, category, value) VALUES ('$userid','$trans_date','$trans_category', '$trans_amount')";
+        $sql = "INSERT INTO transactions (user_id, date, category_id, value) VALUES ('$userid','$trans_date','$trans_category', '$trans_amount')";
         break;
     
     case 'edit':
-        $sql = "UPDATE transactions SET value='$trans_amount', date='$trans_date', category='$trans_category' WHERE user_id='$userid' AND transaction_id='$id'";
+        $sql = "UPDATE transactions SET value='$trans_amount', date='$trans_date', category_id='$trans_category' WHERE user_id='$userid' AND transaction_id='$id'";
         break;
 
     case 'delete':
